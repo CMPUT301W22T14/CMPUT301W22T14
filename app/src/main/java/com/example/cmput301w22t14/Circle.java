44e6ee55891765c97e2cfdb5ff683360564d1207
xiaofeiyu2019 <yufei24@ualberta.ca>
2022-02-04 08:55:00 -0700

    add Circle

--- app/src/main/java/com/example/cmput301w22t14/Circle.java
+++ app/src/main/java/com/example/cmput301w22t14/Circle.java
@@ -1,8 +1,10 @@
 package com.example.cmput301w22t14;
 
 public class Circle extends Shape {
+    private int radius;
 
-    public Circle(int x, int y) {
+    public Circle(int x, int y, int radius) {
         super(x, y);
+        this.radius = radius;
     }
 }
