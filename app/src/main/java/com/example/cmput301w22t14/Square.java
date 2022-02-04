a6451af153fb165ce5f68b5289587f335d349e33
Edennnnnnnnnn <zhou9@ualberta.ca>
2022-02-04 15:26:43 -0700

    Eden adds the Square class

---
+++ app/src/main/java/com/example/cmput301w22t14/Square.java
@@ -0,0 +1,17 @@
+package com.example.cmput301w22t14;
+
+public class Square extends Shape {
+    private int sideLength;
+
+    public Square(int x) {
+        super(x, x);
+    }
+
+    public void setSideLength(int sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    public int getSideLength() {
+        return sideLength;
+    }
+}
