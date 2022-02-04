60f52d3d57e51fd3dc8a594f7c6f380498df5c81
TaylorGraff <46497329+TaylorGraff@users.noreply.github.com>
2022-02-04 12:33:25 -0700

    Resolved conflict in branch "taylor".

--- app/src/main/java/com/example/cmput301w22t14/Shape.java
+++ app/src/main/java/com/example/cmput301w22t14/Shape.java
@@ -4,6 +4,7 @@ public abstract class Shape {
     private int x;
     private int y;
     private String color;
+
     public Shape(int x, int y, String color) {
         this.x = x;
         this.y = y;
